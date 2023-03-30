99cbb4685bf14b76eea2846818281fabce1bdb88
Vladislav Ryzhov <vladislavrzhv@gmail.com>
2023-03-30 20:01:36 +0300

    feat: add tables 'case_notes', 'claim', 'users', 'patient_case'

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import asyncio
 import os
 
 from pathlib import Path
+
+import MySQLdb
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import KeyboardButton
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
@@ -62,28 +64,8 @@ async def handle_message(message: types.Message) -> None:
 
     current_history = user_histories[message.from_user.id]
 
-    # Define the prompt template
-    template = """You are a doctor assistant bot of the True Billing company. Your task is to use the database to give 
-    accurate answers to user requests. Answer the following questions as best you can. 
-
-    Given an input question, first create a syntactically correct query to run, then look at the results of 
-    the query and return the answer. Unless the user specifies in his question a specific number of examples he 
-    wishes to obtain, always limit your query to at most top_k results. You can order the results by a relevant 
-    column to return the most interesting examples in the database. Never query for all the columns from a specific 
-    table, only ask for a the few relevant columns given the question. Pay attention to use only the column names 
-    that you can see in the schema description. Be careful to not query for columns that do not exist. Also, 
-    pay attention to which column is in which table. 
-
-    User: {query}
-    Doctor assistant: """
-
-    # define prompt with template
-    #prompt = PromptTemplate(
-    #    input_variables=["query"],
-    #    template=template
-    #)
-
-    tables = ['patients', 'facility', 'provider_facility', 'patient_visit', 'patient_visit_cpt', 'patient_visit_icd']
+    tables = ['patients', 'facility', 'provider_facility', 'patient_visit', 'patient_visit_cpt', 'patient_visit_icd',
+              'case_notes', 'claim', 'users', 'patient_case']
 
     db = SQLDatabase.from_uri(SQL_DB.get_uri(), include_tables=tables)
     model = 'text-davinci-003'  # 'gpt-3.5-turbo'
@@ -101,9 +83,11 @@ async def handle_message(message: types.Message) -> None:
     # generate response
     await bot.send_chat_action(message.from_user.id, action=types.ChatActions.TYPING)
     formatted_msg = format_date(message.text)
-    print('\n\n\n', formatted_msg, '\n\n\n')
+    #print('\n\n\n', formatted_msg, '\n\n\n')
     try:
         await bot.send_message(message.from_user.id, text=chatgpt_chain.run(formatted_msg))
+    except MySQLdb.ProgrammingError:
+        await bot.send_message(message.from_user.id, text="Please, rephrase your question")
     except Exception as e:
         print(e)
         await bot.send_message(message.from_user.id, text="Please, provide full date")
